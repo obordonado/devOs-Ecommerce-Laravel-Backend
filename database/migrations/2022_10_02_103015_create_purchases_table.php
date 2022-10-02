bd37af597bede5567eada407cfd7a21e25062ff0
Oscar <obordonado@protonmail.com>
2022-10-02 12:31:24 +0200

    feat: created table purchases

---
+++ database/migrations/2022_10_02_103015_create_purchases_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            Schema::create('product_sale', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('sale_id');
+                $table->unsignedBigInteger('product_id');
+                $table->integer('quantity')->nullable();
+                $table->float('price')->nullable();
+                $table->timestamps();
+                $table->unique(['sale_id','product_id']);
+    
+                $table->foreign('sale_id')->references('id')->on('sales')->onDelete('cascade');
+                $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
