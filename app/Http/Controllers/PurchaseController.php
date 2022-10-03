7ab36bfdf1f2ec7089718fe367d410f80f118b82
Oscar <obordonado@protonmail.com>
2022-10-03 11:28:16 +0200

    feat: added result of total_price in sales table

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Spatie\FlareClient\Time\Time;
 
 class PurchaseController extends Controller
 {
@@ -37,42 +38,64 @@ class PurchaseController extends Controller
                     400
                 );
             };
-
-
+            /// crea id en sales para que no de error  restraint
             $sale = new Sale();
             $sale->user_id = $userId;     
             $sale->save();
 
 
+            /// coge y ordena valores de id en sales
             $sale_id = DB::table('sales')
             ->orderByDesc('updated_at')
             ->latest()
             ->get('id')
             ->first();
-            
-            Log::info($sale_id->id);
+           
 
+            /// Devuelve el valor del último id generado en tabla sales.
             $valor = $sale_id->id;
+            Log::info('usuario genera nuevo id '.$sale_id->id.' en sales ');
 
-            if($valor){
+            /// Devuelve el valor del id del usuario que hace la compra.
+            $user = DB::table('sales')
+            ->where('user_id','=', $userId)
+            ->get()
+            ->first();
+            Log::info('usuario que hace la compra '.$user->id);
+
+
+            if($valor && $user){
 
                 $valor = $valor-$valor+1;
             };
-
-            
+           
             $product_id = $request->input('product_id');
             $quantity = $request->input('quantity');
             $price = $request->input('price');
     
-            $purchase =  new Purchase();
+            $purchase = new Purchase();
             $purchase->sale_id = $valor;
             $purchase->product_id = $product_id;
             $purchase->quantity = $quantity;
             $purchase->price = $price;
             $purchase->save();
 
+            $sum = DB::table('purchases')
+            ->where('sale_id','=', $valor)
+            ->sum('price');
+            Log::info('log de $sum '.$sum);
 
-
+            $total_price = DB::table('sales')
+            ->where('id', '=', $valor)->latest()
+            ->where('user_id','=',$userId)
+            ->insertGetId(array
+            (               
+                'user_id' => $userId,
+                'total_price' => $sum,
+                "created_at" => \Carbon\Carbon::now(), # new \Datetime()
+                "updated_at" => \Carbon\Carbon::now(),  # new \Datetime()
+            )                
+            );
 
 
             Log::info('User id '.$userId.' created a purchase correctly.');
