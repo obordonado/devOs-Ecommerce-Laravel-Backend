a6c8309598d52f1fcf4202e66c3b08460f3394c2
Oscar <obordonado@protonmail.com>
2022-10-04 19:17:45 +0200

    feat: formatted code

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -47,19 +47,19 @@ class PurchaseController extends Controller
         $sale->user_id = $userId;
         $sale->save();
 
-          // Getting last sale id.
+        // Getting last sale id.
         $sale_id = DB::table('sales')
-        ->orderByDesc('updated_at')
-        ->latest()
-        ->get('id')
-        ->first();
+          ->orderByDesc('updated_at')
+          ->latest()
+          ->get('id')
+          ->first();
 
         // Returns last id value in sales table.
         $valor = $sale_id->id;
-        Log::info('usuario ' . $userId . ' ("doesntExist" genera nuevo id en sales ' . $sale_id->id . ' en sales ');
+        Log::info('usuario ' . $userId . ' "doesntExist" genera nuevo id en sales ' . $sale_id->id . ' en sales ');
 
         // Controlling that sale id is correct in purchases.
-        if ($valor == 1) { 
+        if ($valor == 1) {
 
           $valor = $valor;
         } else if ($valor / 2 == 0) {
@@ -69,9 +69,9 @@ class PurchaseController extends Controller
 
         // Returns value of the userid that is purchasing.
         $user = DB::table('sales')
-        ->where('user_id', '=', $userId)
-        ->get()
-        ->first();
+          ->where('user_id', '=', $userId)
+          ->get()
+          ->first();
         Log::info('usuario que hace la compra ' . $user->id);
 
         $product_id = $request->input('product_id');
@@ -88,35 +88,34 @@ class PurchaseController extends Controller
         $purchase->save();
 
         $sum = DB::table('purchases')
-        ->where('sale_id', '=', $valor)
-        ->where('user_id', '=', $userId)
-        ->sum('price');
-        Log::info('log de $sum '.$sum);
+          ->where('sale_id', '=', $valor)
+          ->where('user_id', '=', $userId)
+          ->sum('price');
+        Log::info('log de $sum ' . $sum);
 
         // insert total price in last sales id with last userId.
         $total_price = DB::table('sales')
-        ->where('user_id', '=', $userId)
-        ->where('id', '=', $sale_id->id)->latest()
-        ->update(['total_price' => $sum]);
+          ->where('user_id', '=', $userId)
+          ->where('id', '=', $sale_id->id)->latest()
+          ->update(['total_price' => $sum]);
       } else {
-
         if (DB::table('sales')->where('user_id', $userId)->exists()) {
 
           // Getting last sale id.
           $sale_id = DB::table('sales')
-          ->where('user_id','=', $userId)
-          ->where('status','=', 'in progress')
-          ->orderByDesc('updated_at')
-          ->latest()
-          ->get('id')
-          ->first();
+            ->where('user_id', '=', $userId)
+            ->where('status', '=', 'in progress')
+            ->orderByDesc('updated_at')
+            ->latest()
+            ->get('id')
+            ->first();
 
           // Returns last id value in sales table.
           $valor = $sale_id->id;
-          Log::info('Usuario id '.$userId.' ( de "exists") genera nuevo id '.$sale_id->id .' en tabla sales ');
+          Log::info('Usuario id ' . $userId . ' ( de "exists") genera nuevo id ' . $sale_id->id . ' en tabla sales ');
 
           // Controlling that sale id is correct in purchases.
-          if ($valor == 1) { 
+          if ($valor == 1) {
 
             $valor = $valor;
           } else if ($valor / 2 == 0) {
@@ -126,17 +125,16 @@ class PurchaseController extends Controller
 
           // Returns value of the userid that is purchasing AND is still in progress.
           $user = DB::table('sales')
-          ->where('user_id', '=', $userId)
-          ->where('status','=','in progress')/// <- tocado aquí añadido linea completa.
-          ->get()
-          ->first();
+            ->where('user_id', '=', $userId)
+            ->where('status', '=', 'in progress') /// <- tocado aquí añadido linea completa.
+            ->get()
+            ->first();
           Log::info('usuario que hace la compra ' . $user->id);
 
           $saleId = DB::table('sales')
-          ->where('user_id', '=', $userId)
-          ->get('id')
-          ->first();
-          
+            ->where('user_id', '=', $userId)
+            ->get('id')
+            ->first();
 
           $product_id = $request->input('product_id');
           $quantity = $request->input('quantity');
@@ -151,16 +149,16 @@ class PurchaseController extends Controller
           $purchase->save();
 
           $sum = DB::table('purchases')
-          ->where('sale_id', '=', $valor)
-          ->where('user_id', '=', $userId)
-          ->sum('price');
+            ->where('sale_id', '=', $valor)
+            ->where('user_id', '=', $userId)
+            ->sum('price');
           Log::info('log de $sum ' . $sum);
 
           // Inserting total price in last sales id with last userId.
           $total_price = DB::table('sales')
-          ->where('id', '=', $sale_id->id)->latest()
-          ->where('user_id', '=', $userId)
-          ->update(['total_price' => $sum]);
+            ->where('id', '=', $sale_id->id)->latest()
+            ->where('user_id', '=', $userId)
+            ->update(['total_price' => $sum]);
         }
       }
       Log::info('User id ' . $userId . ' created a purchase correctly.');
