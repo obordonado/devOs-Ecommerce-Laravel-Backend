cf54147acdcf57278aa4418cfee45d711844c1a6
Oscar <obordonado@protonmail.com>
2022-10-08 23:22:05 +0200

    feat: added css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./resources/css/welcome.css.css">
     <title>DevOs E-Commerce API</title>
 </head>
 <body>
-    <H1>This is devOs APIRESTful for an E-Commerce</H1>
+    <div id="header">
+        <H1>This is devOs APIRESTful for an E-Commerce</H1>
+    </div>
+    
 </body>
 </html>
