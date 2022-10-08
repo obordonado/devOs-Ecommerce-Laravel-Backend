1293f5cb322e69d83095d28e36ef4e6971658e61
Oscar <obordonado@protonmail.com>
2022-10-08 23:28:31 +0200

    fix: css error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/resources/css/welcome.css">
+    <link rel="stylesheet" href="./resources/css/welcome.css">
     <title>DevOs E-Commerce API</title>
 </head>
 <body>
