35619ca9358f41e225ec1a364372570976e32023
Oscar <obordonado@protonmail.com>
2022-10-08 22:40:55 +0200

    feat: added greeting.blade

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DevOs E-Commerce API</title>
+</head>
+<body>
+    <H1>This is devOs APIRESTful for an E-Commerce</H1>
+</body>
+</html>
