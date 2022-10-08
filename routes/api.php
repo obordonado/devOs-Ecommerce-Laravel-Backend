9a3b61105eb8d8108ea6d98c31a4f9c39fe08542
Oscar <obordonado@protonmail.com>
2022-10-08 23:06:44 +0200

    feat: modified welcome.blade.php

--- routes/api.php
+++ routes/api.php
@@ -27,9 +27,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/', function () {
-    return view('greeting');
-});
 
 
 // ROUTES FOR USERS 
